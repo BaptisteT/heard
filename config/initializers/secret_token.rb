a624fdc9758f4f8cc9f0b18d8f3072b6116670b4
Bastien Beurier <bastienbeurier@gmail.com>
2014-06-17 15:54:47 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Heard::Application.config.secret_key_base = '99149ee82f7b0191a4a53370dcdeafed562addf3ce5e3f331c1891615685e2d49ee6f4957a09c42a2bf42b131e1b00fa2dd6aa712bcbcb3163b52f23b695e0f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Heard::Application.config.session_store :cookie_store, key: '_heard_session'
