f872c2dd4a3f188707a961a81262418402983dac
Baptiste <baptiste.truchot@gmail.com>
2014-06-20 16:35:11 +0200

    private key bug fix

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::MessagesController < Api::V1::ApiController
         #send notif
         sender  = User.find(params[:sender_id])
         message = 'New message from @' + sender.first_name
-        APNS.pem = 'app/assets/HeardCert.pem'
+        APNS.pem = 'app/assets/cert.pem'
         APNS.port = 2195
         APNS.host = 'gateway.push.apple.com' 
         APNS.send_notification(receiver.push_token , :alert => message, :badge => 1)
