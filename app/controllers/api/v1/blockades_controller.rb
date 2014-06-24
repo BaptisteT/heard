e4734a473fad6eeb5d91d3c1d88f0bd04c528435
Baptiste <baptiste.truchot@gmail.com>
2014-06-24 23:05:50 +0200

    i’m stupid

--- app/controllers/api/v1/blockades_controller.rb
+++ app/controllers/api/v1/blockades_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::BlockadesController < Api::V1::ApiController
 
   def create
     blockade = Blockade.new
-    blockade.blocked_id = current_user.id
-    blockade.blocker_id = params[:blocker_id]
+    blockade.blocker_id = current_user.id
+    blockade.blocked_id = params[:blocked_id]
     if blockade.save
       render json: { result: { message: ["Blockade successfully saved"] } }, status: 201
     else 
