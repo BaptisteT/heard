dc8d4240a9e02ec38513eda5d430e8c5b7499cd0
Baptiste <baptiste.truchot@gmail.com>
2014-06-24 22:39:53 +0200

    Remove blockade_params

--- app/controllers/api/v1/blockades_controller.rb
+++ app/controllers/api/v1/blockades_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::BlockadesController < Api::V1::ApiController
 
   def create
-    blockade = Blockade.new(blockade_params)
+    blockade = Blockade.new
+    blockade.blocked_id = current_user.id
     blockade.blocker_id = current_user.id
     if blockade.save
       render json: { result: { message: ["Blockade successfully saved"] } }, status: 201
@@ -9,10 +10,5 @@ class Api::V1::BlockadesController < Api::V1::ApiController
       render json: { errors: { internal: blockade.errors } }, :status => 500
     end
   end
-
-  private
-
-    def blockade_params
-      params.permit(:blocked_id)
-    end 
+ 
 end
