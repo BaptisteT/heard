e88f2f5c1ee33678bfa76a77db210cfb17b87ebd
Bastien Beurier <bastienbeurier@gmail.com>
2014-07-03 12:38:49 -0700

    Redirect to dl beta ios

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ActionController::Base
   def beta
+  	redirect_to "itms-services://?action=download-manifest&url=https://s3.amazonaws.com/Heard_inHouse/manifest.plist"
   end
 
   def index
