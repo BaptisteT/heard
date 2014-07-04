047d92ca54c1c62b20f11ed5db00bf87bba1804e
Baptiste <baptiste.truchot@gmail.com>
2014-07-04 12:46:23 +0200

    bug fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Constant to be used in config/environments are defined here
+SENDER_EMAIL_ = "info@waved.io"
+
 # Initialize the Rails application.
 Heard::Application.initialize!
 
-# Constant to be used in config/environments are defined here
-SENDER_EMAIL_ = "info@waved.io"
+
