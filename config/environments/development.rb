58ee9c13de76b371456f0d48ca9f2dd28147f08e
Bastien Beurier <bastienbeurier@gmail.com>
2014-07-04 17:01:24 -0700

    Debug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,7 @@ Heard::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+  config.assets.compress = false
   config.assets.debug = true
 
   # Mailer
