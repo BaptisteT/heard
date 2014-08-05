14200c341f2405292f0d2109e86d89807e2bd8bb
Baptiste <baptiste.truchot@gmail.com>
2014-08-05 20:10:35 +0200

    Reformate api check

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -24,11 +24,10 @@ class Api::V1::ApiController < ApplicationController
   # app_version related message
   def obsolete_api
     
-
-    if params[:app_version] && ["1.1.1", "1.1.2"].include?(params[:app_version])
-      render json: {result: { message: "Ok"} }, status: 200
-    else
+    if !params[:app_version] || is_below_threshold(params[:app_version],THRESHOLD)
       render json: {result: { message_type: "Blocking alert", message_content: "Please download the latest version at www.waved.io/beta.", redirect_url: "http://www.waved.io/beta" } }, status: 200 
+    else
+      render json: {result: { message: "Ok"} }, status: 200
     end
     # if params[:app_version] == "1.1"
     #   render json: {result: { message_type: "Informative alert", message_content: "Download the new version", redirect_url: "http://itunes.apple.com/app/id734887535?mt=8" } }, status: 200
@@ -38,4 +37,22 @@ class Api::V1::ApiController < ApplicationController
     #   render json: {result: { message: "Ok"} }, status: 200
     # end
   end
+
+  private
+    def is_below_threshold(app_version,threshold)
+      threshold_array = threshold.split(".").map { |s| s.to_i }
+      version_array = app_version.split(".").map { |s| s.to_i }
+      (0..version_array.count).each do |i|
+        if threshold_array.count < i || threshold_array[i] < version_array[i]
+          return NO
+        else if threshold_array[i] > version_array[i]
+          return YES
+        end
+      end
+      if threshold_array.count > version_array.count
+        return YES
+      else
+        return NO
+      end
+    end
 end
