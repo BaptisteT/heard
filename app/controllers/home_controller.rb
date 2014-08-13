c00af1485dddcb644851826fb5e98ddd8671fab4
Baptiste <baptiste.truchot@gmail.com>
2014-08-13 12:29:18 +0200

    bug fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
   def text_link
     # todo BT check number is valid ?
     invited_number = InvitedNumber.new
-    invited_number.phone_number = params[:phone_number]
+    invited_number.phone_number = params[:number]
     
     if invited_number.save
       begin
