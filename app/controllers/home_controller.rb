2d1d9da7854464856af3748cbd8607dcbabe4048
Baptiste <baptiste.truchot@gmail.com>
2014-08-13 12:22:25 +0200

    bug fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,22 +9,21 @@ class HomeController < ApplicationController
 
   def text_link
     # todo BT check number is valid ?
-    if params[:number]
-      invited_number = InvitedNumber.new
-      invited_number.phone_number = params[:phone_number]
-      
-      if invited_number.save
-        begin
-          client = Twilio::REST::Client.new(TWILIO_SID, TWILIO_TOKEN)
-          client.account.messages.create(
-            from: TWILIO_PHONE_NUMBER,
-            to:   params[:number],
-            body: "Download Waved at " + DOWNLOAD_LINK
-          )
-        rescue Twilio::REST::RequestError => e
-          
-        end
+    invited_number = InvitedNumber.new
+    invited_number.phone_number = params[:phone_number]
+    
+    if invited_number.save
+      begin
+        client = Twilio::REST::Client.new(TWILIO_SID, TWILIO_TOKEN)
+        client.account.messages.create(
+          from: TWILIO_PHONE_NUMBER,
+          to:   params[:number],
+          body: "Download Waved at " + DOWNLOAD_LINK
+        )
+      rescue Twilio::REST::RequestError => e
+        
       end
+    end
 
     redirect_to action:index
   end
