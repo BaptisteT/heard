51a232a957403feeef19dad1a6151b51e0d65ccc
Baptiste <baptiste.truchot@gmail.com>
2014-08-19 12:19:59 +0200

    bug fix 3

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -111,7 +111,7 @@ class Api::V1::MessagesController < Api::V1::ApiController
 
   def retrieve_conversation
     messages = Message.where("(sender_id = ? and receiver_id = ?) or (sender_id = ? and receiver_id = ?)",
-      params[:first_user_id],params[:receiver_id],params[:receiver_id],params[:first_user_id])
+      params[:first_user_id],params[:second_user_id],params[:second_user_id],params[:first_user_id])
     render json: { result: { messages: Message.response_messages(messages)} }, status: 201
   end
 
