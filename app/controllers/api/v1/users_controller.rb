d76192cb5f1c00bb30a9d5384172853baeee0486
Baptiste <baptiste.truchot@gmail.com>
2014-09-20 19:26:04 -0700

    fix 3

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -157,8 +157,8 @@ class Api::V1::UsersController < Api::V1::ApiController
 
   def get_contacts_and_futures
     contact_numbers = []
-    # ar = JSON.parse(params[:contact_infos])
-    params[:contact_infos].each { |number, contact_info|
+    dict = JSON.parse(params[:contact_infos])
+    dict.each { |number, contact_info|
       contact_numbers += number
     }
     # Get contacts (except blocked)
