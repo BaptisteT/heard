a1ecfdd30b4b324ce278e475a6c7882f36970299
Baptiste <baptiste.truchot@gmail.com>
2014-10-23 18:59:52 +0200

    send back more info for groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,10 +7,20 @@ class Group < ActiveRecord::Base
     self.users.pluck(:id)
   end
 
+  def member_first_names
+    self.users.pluck(:first_name)
+  end
+
+  def member_last_names
+    self.users.pluck(:last_name)
+  end
+
   def group_info
     { id: self.id,
       group_name: self.name,
-      member_ids: self.member_ids }
+      member_ids: self.member_ids 
+      member_first_names: self.member_first_names
+      member_last_names: self.member_last_names}
   end
 
   def self.group_info(groups)
