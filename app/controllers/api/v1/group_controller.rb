dbd5791ad378c436203a286f371c2f7a408812e3
Baptiste <baptiste.truchot@gmail.com>
2014-10-23 13:54:22 +0200

    leave fix

--- app/controllers/api/v1/group_controller.rb
+++ app/controllers/api/v1/group_controller.rb
@@ -48,10 +48,7 @@ class Api::V1::GroupsController < Api::V1::ApiController
   end
 
   def leave_group
-    membership = GroupMembership.where(user_id:current_user.id and group_id:params[:group_id])
-    if (membership)
-      membership.destroy
-    end
+    GroupMembership.destroy_all(user_id:current_user.id and group_id:params[:group_id])
     group = Group.find(params[:group_id])
     new_members_number = GroupMembership.where(group_id:params[:group_id]).count
     group.update_attributes(members_number:new_members_number)
