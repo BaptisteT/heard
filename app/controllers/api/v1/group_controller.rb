36ded63e5912eddfa31e5a1b8431ac0a62819a66
Baptiste <baptiste.truchot@gmail.com>
2014-11-04 11:47:20 +0100

    bug fix

--- app/controllers/api/v1/group_controller.rb
+++ app/controllers/api/v1/group_controller.rb
@@ -66,45 +66,46 @@ class Api::V1::GroupsController < Api::V1::ApiController
     # check not already a member
     if group.member_ids.include?(params[:new_member_id].to_i)
       render json: { errors: {message:["Already a member"]} }, status: 500
-    end
-    membership = GroupMembership.new
-    membership.user_id = params[:new_member_id]
-    membership.group_id = params[:group_id]
-    membership.save!
-    new_members_number = GroupMembership.where(group_id:group.id).count
-    group.update_attributes(members_number:new_members_number)
-    #send notification
-    pusher_beta = Grocer.pusher(certificate: 'app/assets/cert.pem', passphrase:  "djibril", gateway: "gateway.push.apple.com")
-    pusher_prod = Grocer.pusher(certificate: 'app/assets/WavedProdCert&Key.pem', passphrase: ENV['CERT_PASS'], gateway: "gateway.push.apple.com")
-    notifications_beta = []
-    notifications_prod = []
+    else
+      membership = GroupMembership.new
+      membership.user_id = params[:new_member_id]
+      membership.group_id = params[:group_id]
+      membership.save!
+      new_members_number = GroupMembership.where(group_id:group.id).count
+      group.update_attributes(members_number:new_members_number)
+      #send notification
+      pusher_beta = Grocer.pusher(certificate: 'app/assets/cert.pem', passphrase:  "djibril", gateway: "gateway.push.apple.com")
+      pusher_prod = Grocer.pusher(certificate: 'app/assets/WavedProdCert&Key.pem', passphrase: ENV['CERT_PASS'], gateway: "gateway.push.apple.com")
+      notifications_beta = []
+      notifications_prod = []
 
-    group.users(true).each { |user|
-      if user.id == membership.user_id
-        text = current_user.first_name + " just added you to the group " + group.name
-      else
-        text = current_user.first_name + " just added " + User.find(params[:new_member_id]).first_name + " to the group " + group.name
-      end 
-      if !user.push_token.blank? and user.id != current_user.id
-        user.update_attributes(:retrieve_contacts => true)
-        notification = Grocer::Notification.new(
-                          device_token:      user.push_token,
-                          alert:             text,
-                          expiry:            Time.now + 60*600,
-                          sound:             'default')
-        if user.is_beta_tester
-          notifications_beta += [notification]
+      group.users(true).each { |user|
+        if user.id == membership.user_id
+          text = current_user.first_name + " just added you to the group " + group.name
         else
-          notifications_prod += [notification]
+          text = current_user.first_name + " just added " + User.find(params[:new_member_id]).first_name + " to the group " + group.name
+        end 
+        if !user.push_token.blank? and user.id != current_user.id
+          user.update_attributes(:retrieve_contacts => true)
+          notification = Grocer::Notification.new(
+                            device_token:      user.push_token,
+                            alert:             text,
+                            expiry:            Time.now + 60*600,
+                            sound:             'default')
+          if user.is_beta_tester
+            notifications_beta += [notification]
+          else
+            notifications_prod += [notification]
+          end
         end
+      }
+      notifications_prod.each do |notification|
+        pusher_prod.push(notification)
       end
-    }
-    notifications_prod.each do |notification|
-      pusher_prod.push(notification)
+      notifications_beta.each do |notification|
+        pusher_beta.push(notification)
+      end
+      render json: { result: {is_full:false, group:group.group_info} }, status: 201
     end
-    notifications_beta.each do |notification|
-      pusher_beta.push(notification)
-    end
-    render json: { result: {is_full:false, group:group.group_info} }, status: 201
   end
 end
