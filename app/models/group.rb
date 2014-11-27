d1d071d9836e819ec9ab3e0cc2fb97f311037a8e
Baptiste <baptiste.truchot@gmail.com>
2014-11-27 18:51:40 +0100

    allow emoji in group name

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ActiveRecord::Base
   has_many :group_memberships, :class_name => 'GroupMembership'
   has_many :users, through: :group_memberships
   validates :name, presence: true
+  stores_emoji_characters :name
 
   def member_ids
     self.users.pluck(:id)
